36c64a5395a8bb44d984c8e6a7c4fbd9995ccbb1
Ryan McKinney <ryangmckinney@hotmail.com>
2017-02-21 11:11:01 -0800

    Added JAR

--- src/Main.java
+++ src/Main.java
@@ -18,8 +18,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-            launch(args);
-        }
+    public static void main(String[] args) { launch(args); }
 }
 
